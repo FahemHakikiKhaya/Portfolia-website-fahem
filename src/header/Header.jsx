1abf592f02db1d74fadf9bf294c780d33d0fd3b6
FahemHakikiKhaya <hakikihaya@gmail.com>
2021-11-27 10:58:26 +0700

    Header-Navigation-Raw

---
+++ src/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HeaderStyle.css'
+
+function Header() {
+    return (
+        <div>
+            <div className="Header">
+                <h4 className="body-font">Been on The Toughest Road</h4>
+                <h1 className="title-font">The Good Things Only</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './NavBarStyle.css'
+
+function NavBar() {
+    return (
+
+                <div className="Nav-Bar body-font">
+                <a href="" className="text-decoration-none text-black border-inline">Home</a>
+                <a href="" className="text-decoration-none text-black">About</a>
+                <a href="" className="text-decoration-none text-black border-inline">My Blog</a>
+                <a href="" className="text-decoration-none text-black border-right">Contact</a>
+           
+                <div className="input-group rounded search-button">
+                    <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
+                    aria-describedby="search-addon" />
+                    <span className="input-group-text border-0" id="search-addon">
+                    <i className="fas fa-search"></i>
+                    </span>
+                </div>
+
+                <div className="Icon-list">
+                <a href="https:www.instagram.com/fahemkhaya/"><i className="fa fa-instagram"></i></a>
+                <a href=""><i className="fa fa-twitter-square"></i></a>
+                <a href=""><i className="fa fa-facebook"></i></a>
+                <a href=""><i className="fa fa-pinterest"></i></a>
+                </div>
+         </div>
+      
+    )
+}
+
+export default NavBar
